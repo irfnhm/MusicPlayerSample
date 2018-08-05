be96d4e5cf093fd95eb7f7934fee316dd6b3c534
Irfan Hamid <irfnhm@gmail.com>
2018-08-05 19:45:14 +0530

    Some fixes

--- app/src/main/java/com/example/irfan/musicplayersample/MainActivity.java
+++ app/src/main/java/com/example/irfan/musicplayersample/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
@@ -22,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "I'm done", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
